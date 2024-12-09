b7a29b27ffa04799b783269b876f4c1fbf20fa27
Helysson16 <hdfs1@aluno.ifal.edu.br>
2024-12-09 09:20:34 -0300

    resolvendo a issue de atualizr pct e rm import

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Importação das funções necessárias do Firebase
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth } from 'firebase/auth';
+import { getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-// Your web app's Firebase configuration
-import { getAuth } from "firebase/auth";
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBVnq99-E-egRJR-4OQPbJsv-UHOZZSqio",
   authDomain: "ecomanager-81ce4.firebaseapp.com",
@@ -19,10 +15,10 @@ const firebaseConfig = {
   measurementId: "G-REDEYZZV0G"
 };
 
-// Initialize Firebase
+// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
+// Inicializar o Firebase Auth com persistência em AsyncStorage
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
